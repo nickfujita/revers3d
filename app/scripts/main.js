ad7517dc5c5fd540e386b88d572fcd017e74d546
Stephen Wu <stefan.wu@gmail.com>
2016-03-02 20:22:01 -0800

    separated view modes for desktop and mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,16 +26,12 @@
   light.position.set( 0.5, 1, 0.75 );
   scene.add( light );
 
-  window.controls = new THREE.PointerLockControls( camera );
-  // window.controls = new THREE.DeviceOrientationControls(camera, true);
-  scene.add( controls.getObject() );
 
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setClearColor( 0xffffff );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  effect = new THREE.StereoEffect(renderer);
-  document.body.appendChild( renderer.domElement );
+  var mono = new THREE.WebGLRenderer();
+  mono.setClearColor( 0xffffff );
+  mono.setPixelRatio( window.devicePixelRatio );
+  mono.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( mono.domElement );
 
   // Register event listeners
   // window.addEventListener('deviceorientation', setOrientationControls, true);
@@ -44,30 +40,68 @@
   document.addEventListener( 'keydown', onKeyDown, false );
   document.addEventListener( 'keyup', onKeyUp, false );
 
-  var vrButton = document.getElementById('vr-icon');
-  var screenButton = document.getElementById('screen-icon');
-  vrButton.addEventListener( 'click', toggleVR, false );
-  screenButton.addEventListener( 'click', toggleVR, false );
+  /*
+  ========================================
+      Initialization
+  ========================================
+   */
+  var render, update;
 
+  if(isMobile()) {
+    var vrButton = document.getElementById('vr-icon');
+    var screenButton = document.getElementById('screen-icon');
+    vrButton.style.display = "block";
+    vrButton.addEventListener( 'click', toggleFullScreen.bind(null, enterVR, exitVR), false );
+    screenButton.addEventListener( 'click', toggleFullScreen.bind(null, enterVR, exitVR), false );
+
+    window.controls = new THREE.DeviceOrientationControls(camera, true);
+    var stereo = new THREE.StereoEffect(mono);
+
+    instructions.style.display = 'none';
+    blocker.style.display = 'none';
+
+    render = stereo.render.bind( stereo, scene, camera );
+    update = controls.update.bind( controls );
+  }
+  else {
+    enablePointerLock();
+    window.controls = new THREE.PointerLockControls( camera );
+    scene.add( controls.getObject() );
+
+    render = mono.render.bind( mono, scene, camera );
+    update = camera.updateProjectionMatrix.bind( camera );
+  }
+
+  window.requestAnimationFrame( animate );
+
+  /*
+  ========================================
+      Sockets
+  ========================================
+   */
 
   window.socket = io('http://localhost:3030');
   initBoard();
   var prevTime = performance.now();
-  window.requestAnimationFrame( animate );
 
 
   /*
   ========================================
-      Animation
+      Animation Loop
   ========================================
    */
-  var INTERSECTED;
 
+  // Movement
   var velocity = new THREE.Vector3();
   var moveForward = false;
   var moveBackward = false;
   var moveLeft = false;
   var moveRight = false;
+
+  // Set sight ray
+  var focus, intersects;
+  var sightline = new THREE.Raycaster();
+  var mouse = new THREE.Vector2();
 
   // FPS
   var fps = new Stats();
@@ -85,7 +119,8 @@
     // Begin FPS calculation
     fps.begin();
 
-    if ( controls.enabled ) {
+    // Allow movement for development
+    if ( !stereo ) {
       var delta = ( time - prevTime ) / 1000;
 
       velocity.x -= velocity.x * 10.0 * delta;
@@ -99,38 +134,12 @@
       controls.getObject().translateX( velocity.x * delta );
       controls.getObject().translateZ( velocity.z * delta );
 
-      camera.updateProjectionMatrix();
-
       prevTime = time;
     }
-    // controls.update();
 
-    var sightline = new THREE.Raycaster();
-    var mouse = new THREE.Vector2();
-
-    // update the picking ray with the camera and mouse position
-    sightline.setFromCamera( mouse, camera );
-
-    // calculate board tiles intersecting the picking ray
-    // TODO: change scene.children to board
-    var intersects = sightline.intersectObjects( scene.children );
-
-    if ( intersects.length > 0 ) {
-      if ( INTERSECTED != intersects[ 0 ].object ) {
-        if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-        // Save previous properties of intersected object to restore its properties on blur
-        INTERSECTED = intersects[ 0 ].object;
-        INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-        INTERSECTED.material.emissive.setHex( PLAYER_COLORS[playerTurn] );
-      }
-    } else {
-      if(INTERSECTED) {
-        // Restore previous properties of intersection
-        if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-        INTERSECTED = null;
-      }
-    }
-    effect.render( scene, camera );
+    update();
+    findIntersects();
+    render();
 
     // End FPS calculation
     fps.end();
@@ -143,55 +152,32 @@
       Event Handlers
   ========================================
    */
-  function setOrientationControls(e) {
-    if (!e.alpha) {
-      return;
-    }
+  // function setOrientationControls(e) {
+  //   if (!e.alpha) {
+  //     return;
+  //   }
 
-    controls = new THREE.DeviceOrientationControls(camera, true);
-    controls.connect();
-    controls.update();
-    element.addEventListener('click', fullscreen, false);
+  //   controls = new THREE.DeviceOrientationControls(camera, true);
+  //   controls.connect();
+  //   controls.update();
+  //   element.addEventListener('click', fullscreen, false);
 
-    window.removeEventListener('deviceorientation', setOrientationControls, true);
-  }
+  //   window.removeEventListener('deviceorientation', setOrientationControls, true);
+  // }
 
-  function fullscreen() {
-    if (container.requestFullscreen) {
-      container.requestFullscreen();
-    } else if (container.msRequestFullscreen) {
-      container.msRequestFullscreen();
-    } else if (container.mozRequestFullScreen) {
-      container.mozRequestFullScreen();
-    } else if (container.webkitRequestFullscreen) {
-      container.webkitRequestFullscreen();
-    }
-  }
 
-  function toggleVR(event) {
-    event.stopPropagation();
-
-    if(vrButton.style.display === "none") {
-      vrButton.style.display = "block";
-      screenButton.style.display = "none";
-    }
-    else {
-      vrButton.style.display = "none";
-      screenButton.style.display = "block";
-    }
-  }
 
   function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    mono.setSize( window.innerWidth, window.innerHeight );
   }
 
   function onClick( event ) {
     if(window.controls.enabled) {
-      // if(INTERSECTED.material.emissive.getHex())
-      INTERSECTED.currentHex = PLAYER_COLORS[playerTurn];
+      // if(focus.material.emissive.getHex())
+      focus.currentHex = PLAYER_COLORS[playerTurn];
 
       playerTurn = 1 - playerTurn;
     }
@@ -258,4 +244,90 @@
         break;
     }
   };
+
+  /*
+  ========================================
+      Helpers
+  ========================================
+   */
+
+  function isMobile() {
+    try{ document.createEvent("TouchEvent"); return true; }
+    catch(e){ return false; }
+  }
+
+  function toggleFullScreen(enterFS, exitFS, event) {
+    event.stopPropagation();
+
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+      // Enter fullscreen
+      requestFullScreen.call(docEl);
+      if(enterFS) enterFS();
+    }
+    else {
+      if(exitFS) exitFS();
+      // Exit fullscreen
+      cancelFullScreen.call(doc);
+    }
+  }
+
+  function enterVR() {
+    vrButton.style.display = "none";
+    screenButton.style.display = "block";
+
+    // scene.remove( controls.getObject() );
+    // vrButton.style.display = "block";
+    // screenButton.style.display = "none";
+
+    // controls = new THREE.DeviceOrientationControls(camera, true);
+    // stereo = new THREE.StereoEffect(mono);
+
+    // render = stereo.render.bind( stereo, scene, camera );
+    // update = controls.update.bind( controls );
+  }
+
+  function exitVR() {
+    vrButton.style.display = "block";
+    screenButton.style.display = "none";
+
+    // vrButton.style.display = "none";
+    // screenButton.style.display = "block";
+
+    // controls = new THREE.PointerLockControls( camera );
+    // scene.add( controls.getObject() );
+
+    // render = mono.render.bind( mono, scene, camera );
+    // update = camera.updateProjectionMatrix.bind( camera );
+  }
+
+  function findIntersects() {
+    sightline.setFromCamera( mouse, camera );
+
+    // calculate board tiles intersecting the picking ray
+    intersects = sightline.intersectObjects( scene.children );
+
+    if ( intersects.length > 0 ) {
+      if ( focus != intersects[ 0 ].object ) {
+        if ( focus ) focus.material.emissive.setHex( focus.currentHex );
+        // Save previous properties of intersected object to restore its properties on blur
+        focus = intersects[ 0 ].object;
+        focus.currentHex = focus.material.emissive.getHex();
+        focus.material.emissive.setHex( PLAYER_COLORS[playerTurn] );
+      }
+    } else {
+      if(focus) {
+        // Restore previous properties of intersection
+        if ( focus ) focus.material.emissive.setHex( focus.currentHex );
+        focus = null;
+      }
+    }
+  }
+
 })();
+
--- test.js
+++ test.js
@@ -1,27 +1,16 @@
-// Calculates all combinations of coordinates (x, y, z) such that the points are
-// centered around (0, 0, 0), which form the surface of a cube. Returns an object
-// where obj[x][y][z] is initialized to a value, init
-function allCombos(tileSize, boardSize, radius, init) {
-  var combos = {};
-  var ordinates = [];
-  var max = (tileSize * boardSize / 2) - (1/2 * tileSize);
+function allCombos(remaining, init) {
+  var objSoFar = {};
 
-  for(var i = -max; i <= max; i+= tileSize) {
-    ordinates.push(i);
-  }
-
-
-  function createCoordinates(ordinates, d) {
-    for(var i = 0; i < ordinates.length; i++) {
-      combos[ordinates] = {};
-    }
-
-    for(var i = 0; i < d; i++) {
-      ordinates.forEach(function(value) {
-        combos[value] = createCoordinates(ordinates, d-1);
-      })
+  if(remaining.length) {
+    for(var i = 0; i < remaining.length; i++) {
+      var r = allCombos(remaining.slice(0, i).concat(remaining.slice(i+1)), init);
+      objSoFar[remaining[i]] = r;
+      objSoFar[-remaining[i]] = r;
     }
   }
+  else {
+    objSoFar = init;
+  }
+
+  return objSoFar;
 }
-
-allCombos(10, 4, 0)
